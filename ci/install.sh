ba15af18d9052026f7be71d82882b60b2e8030bb
Jorge Aparicio <jorge@japaric.io>
2017-06-06 21:56:54 -0500

    less dependencies

--- ci/install.sh
+++ ci/install.sh
@@ -6,9 +6,6 @@ main() {
     cargo install --list | grep "xargo v$vers" || \
         cargo install xargo -f --vers $vers
 
-    cargo install --list | grep cargo-clone || \
-        cargo install cargo-clone
-
     rustup component list | grep 'rust-src.*installed' || \
         rustup component add rust-src
 }
--- ci/script.sh
+++ ci/script.sh
@@ -3,16 +3,23 @@ set -euxo pipefail
 main() {
     local src=$(pwd)
     local td=$(mktemp -d)
+    local version=0.1.8
+    local url=https://github.com/japaric/cortex-m-quickstart/archive/v$version.tar.gz
 
     pushd $td
-    cargo clone cortex-m-quickstart --vers 0.1.8
-    cd cortex-m-quickstart
 
-    rm -rf build.rs examples memory.x
+    curl -L $url | tar --strip-components 1 -xz
+
+    rm -rf build.rs examples memory.x src
     ln -s $src/examples .
 
-    cargo add blue-pill --path $src
-    cargo add cortex-m-rtfm --vers 0.1.1
+    cat >>Cargo.toml <<EOF
+[dependencies.blue-pill]
+path = "$src"
+
+[dependencies.cortex-m-rtfm]
+version = "0.1.1"
+EOF
 
     for path in $(ls examples/*); do
         local ex=$(basename $path)
