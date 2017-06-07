c2f399cf0e864bfdeaad90f332c9a5be0bf260f3
Jorge Aparicio <jorge@japaric.io>
2017-06-06 21:45:31 -0500

    add cargo-clone

--- ci/install.sh
+++ ci/install.sh
@@ -4,6 +4,9 @@ main() {
     cargo install --list | grep xargo || \
         cargo install xargo
 
+    cargo install --list | grep cargo-clone || \
+        cargo install cargo-clone
+
     rustup component list | grep 'rust-src.*installed' || \
         rustup component add rust-src
 }
