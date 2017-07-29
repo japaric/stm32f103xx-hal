2b7d5c56b25f4efad6c7c40042f884cbecb47c0b
Jorge Aparicio <jorge@japaric.io>
2017-07-29 01:26:30 -0500

    fix ci/install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -1,13 +1,15 @@
 set -euxo pipefail
 
 main() {
-    local vers=0.3.7
+    if [ $TARGET = thumbv7m-none-eabi ]; then
+        local vers=0.3.7
 
-    cargo install --list | grep "xargo v$vers" || \
-        cargo install xargo -f --vers $vers
+        cargo install --list | grep "xargo v$vers" || \
+            ( cd .. && cargo install xargo -f --vers $vers )
 
-    rustup component list | grep 'rust-src.*installed' || \
-        rustup component add rust-src
+        rustup component list | grep 'rust-src.*installed' || \
+            rustup component add rust-src
+    fi
 }
 
 main
