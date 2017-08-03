584d892f44f39b386bd5615b25d7bd0ef650a195
Jorge Aparicio <jorge@japaric.io>
2017-08-02 21:23:32 -0500

    fix after_success

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,14 +1,18 @@
 set -euxo pipefail
 
 main() {
-    cargo doc
+    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+        return
+    fi
+
+    xargo doc --target $TARGET
 
     mkdir ghp-import
 
     curl -Ls https://github.com/davisp/ghp-import/archive/master.tar.gz | \
         tar --strip-components 1 -C ghp-import -xz
 
-    ./ghp-import/ghp_import.py target/doc
+    ./ghp-import/ghp_import.py target/$TARGET/doc
 
     set +x
     git push -fq https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages && \
