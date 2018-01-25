b5ce99fa7ccaaf57244f8b975f1e49423ba5fe18
Jorge Aparicio <jorge@japaric.io>
2018-01-25 16:27:33 +0100

    impl CountDown for SYST; add listen/unlisten to Timer; add reactive-blinky example

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -4,6 +4,7 @@
 #![deny(warnings)]
 #![no_std]
 
+extern crate cortex_m;
 extern crate stm32f103xx_hal as hal;
 #[macro_use(block)]
 extern crate nb;
@@ -13,23 +14,24 @@ use hal::stm32f103xx;
 use hal::timer::Timer;
 
 fn main() {
-    let p = stm32f103xx::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f103xx::Peripherals::take().unwrap();
 
-    let mut flash = p.FLASH.constrain();
-    let mut rcc = p.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
     // Try a different clock configuration
-    // let clocks = rcc.cfgr.freeze(&mut flash.acr);
-    let clocks = rcc.cfgr
-        .sysclk(64.mhz())
-        .pclk1(32.mhz())
-        .freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // let clocks = rcc.cfgr
+    //     .sysclk(64.mhz())
+    //     .pclk1(32.mhz())
+    //     .freeze(&mut flash.acr);
 
-    let mut gpioc = p.GPIOC.split(&mut rcc.apb2);
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
 
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Try a different timer
-    let mut timer = Timer::tim2(p.TIM2, 1.hz(), clocks, &mut rcc.apb1);
+    // Try a different timer (even SYST)
+    let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
     loop {
         block!(timer.wait()).unwrap();
         led.set_high();
